e748ad6ff85518568c96c7153883556ffea3a558
law909 <balint.lovey@gmail.com>
2023-05-30 02:23:41 +0200

    form reset sikeres felíratkozás után

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('subscribeForm', () => ({
         eventgeneratorList: [],
-        formData: {
-            email: null,
-            eventgenerator: null,
-            parameter: null,
+        formData: null,
+        validation: null,
+        clearFormData() {
+            this.formData = {
+                email: null,
+                eventgenerator: null,
+                parameter: null,
+            }
         },
         init() {
+            this.clearFormData();
             fetch(new URL('/api/eventgenerators', API_URL))
                 .then((response) => response.json())
                 .then((data) => {
@@ -18,6 +23,20 @@ document.addEventListener('alpine:init', () => {
                 method: 'POST',
                 body: new URLSearchParams(this.formData)
             })
+                .then((response) => response.json())
+                .then((respdata) => {
+                    if (respdata.success) {
+                        this.clearFormData();
+                    } else {
+                        this.validation = respdata.fields;
+                        alert('Kérjük javítsa a pirossal jelölt mezőket.');
+                    }
+                })
+                .catch((error) => {
+                    alert(error);
+                })
+                .finally(() => {
+                });
         }
     }));
 });
