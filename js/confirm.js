e3991e57adf12fbb7c0a77cbabccfc18dc673d49
law909 <balint.lovey@gmail.com>
2023-06-01 01:45:13 +0200

    confirmation eredmény csak akkor legyen kiírva, ha kész

--- js/confirm.js
+++ js/confirm.js
@@ -1,6 +1,7 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('confirm', () => ({
         success: null,
+        ready: false,
         init() {
             let myUrlParams = new URLSearchParams(window.location.search);
 
@@ -8,7 +9,10 @@ document.addEventListener('alpine:init', () => {
                 .then((response) => response.json())
                 .then((data) => {
                     this.success = data.success;
-                });
+                })
+                .finally(() => {
+                    this.ready = true;
+                })
         },
     }));
 });
